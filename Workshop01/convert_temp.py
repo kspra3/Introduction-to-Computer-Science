9f2f27e5e8709fee7ae3ed1d41689c9648e98c34
Sharsindra Pratheen <56106960+kspra3@users.noreply.github.com>
2020-02-13 19:15:27 +0800

    Delete convert_temp.py

--- Workshop01/convert_temp.py
+++
@@ -1,7 +0,0 @@
-# K. Sharsindra Pratheen
-# 25636626
-# Advanced Computer Science, Monash University
-
-temp_C = int(input('Enter temperature in Celsius '))
-temp_F = int(9*temp_C/5 + 32)
-print('Temperature in Fahrenheit is ' + str(temp_F))
